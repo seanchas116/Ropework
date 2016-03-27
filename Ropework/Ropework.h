961a386afc6c669587ee9d29b1729b4984a6980b
Ryohei Ikegami <iofg2100@gmail.com>
2016-03-27 15:15:13 +0900

    Init project

---
+++ Ropework/Ropework.h
@@ -0,0 +1,19 @@
+//
+//  Ropework.h
+//  Ropework
+//
+//  Created by Ryohei Ikegami on 2016/03/27.
+//  Copyright © 2016年 seanchas116. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ropework.
+FOUNDATION_EXPORT double RopeworkVersionNumber;
+
+//! Project version string for Ropework.
+FOUNDATION_EXPORT const unsigned char RopeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ropework/PublicHeader.h>
+
+
